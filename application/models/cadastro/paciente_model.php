7d7801ef3158a8071914f51aefb4c8cfe7d2b0fa
Equipe-De-Desenvolvimento <equipe2016gcjh@gmail.com>
2020-05-06 23:33:51 -0300

    La la lal

--- application/models/cadastro/paciente_model.php
+++ application/models/cadastro/paciente_model.php
@@ -547,12 +547,14 @@ class paciente_model extends BaseModel {
         return $return->result();
     }
 
-    function listarparceirosurl() {
+    function listarparceirosurl($parceiro_id = null) {
         $this->db->select('endereco_ip, financeiro_parceiro_id');
         $this->db->from('tb_financeiro_parceiro');
         $this->db->where("ativo", 't');
         $this->db->where("endereco_ip !=", '');
-        // $this->db->where("financeiro_parceiro_id ", $parceiro_id);
+        if($parceiro_id > 0){
+            $this->db->where("financeiro_parceiro_id ", $parceiro_id);
+        }
         $return = $this->db->get();
         return $return->result();
     }
